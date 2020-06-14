ed9f35c7345aa90ffa6e65d328e29fd3afcdf49f
XinUndfined <z26319491z@yahoo.com.tw>
2020-06-14 22:31:15 +0800

    Update 2020-06-14T14:31:12.796Z

---
+++ assets/js/all.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  }
+});
+//# sourceMappingURL=all.js.map
