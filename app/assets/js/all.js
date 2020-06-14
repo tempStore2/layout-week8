c7a404427e25c79fe1be171198f1c32ebe80b326
XinUndfined <z26319491z@yahoo.com.tw>
2020-06-14 22:22:11 +0800

    header banner search

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
-$(document).ready(() => {
-  console.log('Hello Bootstrap4');
-});
+// var swiper = new Swiper(".swiper-container", {
+//   slidesPerView: 1,
+//   loop: true,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2000,
+//     disableOnInteraction: false,
+//   },
+// });
