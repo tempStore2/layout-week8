c528bcb94ea71342df80490bb7f8edb856834b95
XinUndfined <z26319491z@yahoo.com.tw>
2020-06-19 00:15:14 +0800

    choices

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,9 @@
-// var swiper = new Swiper(".swiper-container", {
+// var swiper = new Swiper(".swiper-banner", {
 //   slidesPerView: 1,
 //   loop: true,
 //   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
 //   },
 //   pagination: {
 //     el: ".swiper-pagination",
@@ -16,3 +16,44 @@
 //     disableOnInteraction: false,
 //   },
 // });
+var swiper = new Swiper(".swiper-choices", {
+  slidesPerView: 1.3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
+
+  breakpoints: {
+    375: {
+      slidesPerView: 1.4,
+    },
+    420: {
+      slidesPerView: 1.5,
+    },
+    440: {
+      slidesPerView: 1.6,
+    },
+    460: {
+      slidesPerView: 1.7,
+    },
+    480: {
+      slidesPerView: 1.8,
+    },
+    500: {
+      slidesPerView: 1.9,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2.5,
+    },
+    992: {
+      slidesPerView: 3.5,
+    },
+    1140: {
+      slidesPerView: 4,
+    },
+  },
+});
