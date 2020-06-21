0598272799e4b7d70a8deab15bf75b7cc140901d
XinUndfined <z26319491z@yahoo.com.tw>
2020-06-21 17:31:20 +0800

    Update 2020-06-21T09:31:14.379Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var swiper = new Swiper(".swiper-container", {
+var swiper = new Swiper(".swiper-banner", {
   slidesPerView: 1,
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
   pagination: {
     el: ".swiper-pagination",
@@ -18,4 +18,65 @@ var swiper = new Swiper(".swiper-container", {
     disableOnInteraction: false
   }
 });
+var swiper = new Swiper(".swiper-choices", {
+  slidesPerView: 1.3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next"
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 1.4
+    },
+    420: {
+      slidesPerView: 1.5
+    },
+    440: {
+      slidesPerView: 1.6
+    },
+    460: {
+      slidesPerView: 1.7
+    },
+    480: {
+      slidesPerView: 1.8
+    },
+    500: {
+      slidesPerView: 1.9
+    },
+    520: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 2.5
+    },
+    992: {
+      slidesPerView: 3.5
+    },
+    1140: {
+      slidesPerView: 4
+    }
+  }
+});
+var swiper = new Swiper(".swiper-inspiration", {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2
+    },
+    767: {
+      slidesPerView: 3
+    }
+  }
+});
+
+(function () {
+  $("#date").daterangepicker();
+})();
 //# sourceMappingURL=all.js.map
